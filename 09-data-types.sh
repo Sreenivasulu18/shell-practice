7f49fde4fa71538e268069f13bfc2bc94b0d3217
Sreenivasulu18 <singalasreenivasulu2@gmail.com>
2025-10-14 19:37:50 +0530

    variables

--- 09-data-types.sh
+++ 09-data-types.sh
@@ -3,8 +3,8 @@
 # everything in shell is considered as string
 NUMBER1=100
 NUMBER2=300
+NUMBER3=DevOps
 
-
-SUM=$(($NUMBER1 + $NUMBER2))
+SUM=$(($NUMBER1 + $NUMBER2 + $NUMBER3))
 
 echo "Sum is: ${SUM}"
