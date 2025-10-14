d94786ce14a0662487311ff264589f3d927b955c
Sreenivasulu18 <singalasreenivasulu2@gmail.com>
2025-10-14 16:10:35 +0530

    variables

---
+++ 04-variable.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Arguments
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: welcome to Alaska $PERSON2"
+echo "$PERSON2:: Thank you $PERSON1, finally not on Zoom webinar"
+echo "$PERSON1:: So, do we have a deal"
+echo "$PERSON2:: Yes, if you make Ukraine joins Russia"
