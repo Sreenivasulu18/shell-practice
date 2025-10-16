93e5f74bc63d2a897450ca52e0e6aee56ce4039f
Sreenivasulu18 <singalasreenivasulu2@gmail.com>
2025-10-16 12:33:40 +0530

    Installation

--- 12.Installation.sh
+++ 12.Installation.sh
@@ -4,12 +4,14 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
     echo "ERROR:: Please run this script with root previlage"
+    exit 1 # here exit is other than 0
 fi
 
 dnf install mysql -y
 
 if [ $? -ne 0 ]; then
     echo "ERROR:: Installation MYSQL is FAILURE"
+    exit 1
 else 
     echo "Installation MYSQL is SUCCESS"
 fi
