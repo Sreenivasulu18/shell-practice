a78941987cbb00578425e9a152017be145d01299
Sreenivasulu18 <singalasreenivasulu2@gmail.com>
2025-10-16 12:53:59 +0530

    Installation

---
+++ 13-function.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root previlage"
+fi
+
+VALIDATE(){
+    if [ $? -ne 0 ]; then
+        echo "ERROR:: Installation $1 is FAILURE"
+        exit 1
+    else
+        echo "Installation $1 is SUCCESS"
+}
+
+dnf install mysql -y
+VALIDATE $? "MYSQL"
+
+dnf install nginx -y
+VALIDATE $? "NGINX"
+
+dnf install python3 -y
+VALIDATE $? "Python3"
