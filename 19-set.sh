f1d75a7fc1f37349b81223ce7f7aadf9cbf17a55
Sreenivasulu18 <singalasreenivasulu2@gmail.com>
2025-10-21 16:09:08 +0530

    set

--- 19-set.sh
+++ 19-set.sh
@@ -2,7 +2,12 @@
 
 set -e # e means exit 
 
+error(){
+     echo "There is an error"
+}
+trap error ERR
+
 echo "Hello..."
 echo "Before error..."
-iuwhtljef
+iuwhtljef   # here shell understands there ia an error and signal is ERR
 echo "After error"
