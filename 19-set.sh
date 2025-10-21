2817436b1c52af36f1eb9dd74673328e2f243360
Sreenivasulu18 <singalasreenivasulu2@gmail.com>
2025-10-21 16:19:45 +0530

    set

--- 19-set.sh
+++ 19-set.sh
@@ -2,10 +2,10 @@
 
 set -e # e means exit 
 
-error(){
-     echo "There is an error"
-}
-trap error ERR
+# error(){
+#      echo "There is an error in $LINENO, Command is: $BASH_COMMAND"
+# }
+trap 'echo "There is an error in $LINENO, Command is: $BASH_COMMAND"' ERR
 
 echo "Hello..."
 echo "Before error..."
