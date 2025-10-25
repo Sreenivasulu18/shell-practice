86e21c5053e15b94847d84ad26a10ecd13bc1c17
Sreenivasulu18 <singalasreenivasulu2@gmail.com>
2025-10-25 16:36:36 +0530

    roboshop

---
+++ 24-backup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executed at: $(date)"  | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root previlage"
+    exit 1 # failure is other than 0
+fi 
+
+USAGE(){
+    echo -e "USAGE:: sudo sh 24-backup.sh <SOURCE_DIR> <DEST_DIR> <DAYS>[optional, default 14 days]"
+    exit 1
+}
+
