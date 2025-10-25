60c72df84d7eef5c8798679aa8314ff815c72f87
Sreenivasulu18 <singalasreenivasulu2@gmail.com>
2025-10-25 12:04:55 +0530

    while loop

--- 23-while-loop.sh
+++ 23-while-loop.sh
@@ -4,10 +4,10 @@ count=5
 
 echo "Countdown starts..."
 
-while [ $count -gt 0 ]; then 
+while [ $count -gt 0 ]; 
 do
      echo "Time left: $count"
      sleep 1 # pause for 1 sec
      count=$(($count -1))
-   
+done   
    echo "Times up!"
