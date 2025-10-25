81ffd0cc66d7ff8c9f602499b4e06b6e4354822b
Sreenivasulu18 <singalasreenivasulu2@gmail.com>
2025-10-25 13:08:08 +0530

    while loop

--- 22-delete-old-logs.sh
+++ 22-delete-old-logs.sh
@@ -26,6 +26,8 @@ FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -type f -mtime +14)
 while IFS= read -r filepath 
 do
    echo "deleting the file: $filepath"
+   rm -rf $filepath
+   echo "deleted the file: $filepath"
 
 done <<< $FILES_TO_DELETE
 
