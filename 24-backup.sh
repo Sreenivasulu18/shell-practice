b9570ae58708e20ee64cb2f2752b5bf6409a76b2
Sreenivasulu18 <singalasreenivasulu2@gmail.com>
2025-10-25 17:47:34 +0530

    roboshop

--- 24-backup.sh
+++ 24-backup.sh
@@ -26,28 +26,51 @@ USAGE(){
     exit 1
 }
 
+#### Check SOURCE_DIR and DSET_DIR passed or not
 if [ $# -lt 2 ]; then
     USAGE
 fi 
 
+### Check SOURCE_DIR exist or not ####
 if [ ! -d $SOURCE_DIR ]; then
     echo -e " Source $SOURCE_DIR does not exist $N"
     exit 1
 fi
 
+### Check DEST_DIR exist or not ####
 if [ ! -d $DEST_DIR ]; then
     echo -e "Destination $DEST_DIR does not exist $N"
     exit 1
 fi
 
+### find the files ####
 FILES=$(find $SOURCE_DIR -name "*.log" -type f -mtime +$DAYS)
 
 if [ ! -z "${FILES}" ]; then
+     #### Start Archieving #####
     echo "Files Found: $FILES"
     TIMESTAMP=$(date +%F-%H-%M)
     ZIP_FILE_NAME="$DEST_DIR/app-logs-$TIMESTAMP.Zip"
     echo "Zip file name: $ZIP_FILE_NAME"
     find $SOURCE_DIR -name "*.log" -type f -mtime +$DAYS | zip -@ -j "$ZIP_FILE_NAME"
+
+     ##### Check Archieval SUCCESS or not #####
+    if [ -f $ZIP_FILE_NAME ]
+    then
+        echo -e "Archieval ... $G SUCCESS $N"
+        
+        ### Delete if Success ####
+        while IFS= read -r filepath 
+        do
+          echo "deleting the file: $filepath"
+          rm -rf $filepath
+          echo "deleted the file: $filepath"
+
+        done <<< $FILES
+    else 
+        echo -e "Archieval ... $R FAILURE $N"
+        exit 1
+    fi 
 
 else 
     echo -e "No files to archieve...$Y SKIPPING $N"
